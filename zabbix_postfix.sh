baacaaee93ccf5e9aa72731fe4d3fdfdce24691a
sofibedlu <52102867+sofibedlu@users.noreply.github.com>
2024-01-07 16:52:56 +0300

    Update zabbix_postfix.sh

--- zabbix_postfix.sh
+++ zabbix_postfix.sh
@@ -2,37 +2,19 @@
 
 # DEPS: /usr/sbin/pflogsumm
 # DEPS: pygtail.py
-# DEPS:	bc
-
-# For Debian/Ubuntu
-[ -f /var/log/mail.log ] && MAILLOG=/var/log/mail.log
-# For RHEL/Centos
-[ -f /var/log/maillog ] && MAILLOG=/var/log/maillog
+# DEPS: bc
 
 
-# pygtail from copy within this repository
-if [ -f /usr/local/sbin/pygtail.py ]; then
-  PYGTAIL=/usr/local/sbin/pygtail.py
-# pygtail from Debian 11, Ubuntu 22.04 and later
-elif [ -f /usr/bin/pygtail ]; then
-  PYGTAIL=/usr/bin/pygtail
-# pygtail from Python PIP install to Zabbix user
-elif [ -f /var/lib/zabbix/.local/bin/pygtail ]; then
-  PYGTAIL=/var/lib/zabbix/.local/bin/pygtail
-# pygtail not found
-else
-  >&2 echo "ERROR: pygtail not found!"
-  exit 1
-fi
+MAILLOG=/var/log/mail.log
+
+PYGTAIL=/usr/local/sbin/pygtail.py
 
 FPOS=/tmp/zabbix-postfix-offset.dat
 PFLOGSUMM=/usr/sbin/pflogsumm
 
-# For zabbix-agent
-[ -f /etc/zabbix/zabbix_agentd.conf ] && ZABBIX_CONF=/etc/zabbix/zabbix_agentd.conf
 
-# For zabbix-agent2 
-[ -f /etc/zabbix/zabbix_agent2.conf ] && ZABBIX_CONF=/etc/zabbix/zabbix_agent2.conf
+ZABBIX_CONF=/etc/zabbix/zabbix_agentd.conf
+
 
 function zsend {
   /usr/bin/zabbix_sender -c $ZABBIX_CONF -k $1 -o $2
@@ -51,4 +33,3 @@ zsend pf.held $(echo -e "${DATA}" | grep -m 1 held | cut -f1 -d"h")
 zsend pf.discarded $(echo -e "${DATA}" | grep -m 1 discarded | cut -f1 -d"d")
 zsend pf.bytesreceived $(echo -e "${DATA}" | grep -m 1 "bytes received" | cut -f1 -d"b"|sed -e 's/k/\*1024/g' -e 's/m/\*1048576/g' -e 's/g/\*1073741824/g' |bc)
 zsend pf.bytesdelivered $(echo -e "${DATA}" | grep -m 1 "bytes delivered" | cut -f1 -d"b"|sed -e 's/k/\*1024/g' -e 's/m/\*1048576/g' -e 's/g/\*1073741824/g' |bc)
-
